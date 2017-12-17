92fd1e694890ed9fcb459debe4c676cb108e7b52
chris-malloy <chrismalloymusic@gmail.com>
2017-12-17 17:31:16 -0500

    red black trees

--- red_black_tree/red_black_tree.py
+++ red_black_tree/red_black_tree.py
@@ -1,9 +1,11 @@
 from binarytree import Node, show
 import numpy.random as nprnd
+from collections import deque
 
 class MyBinaryTreeNode(Node):
     def __init__(self, value):
         self.value = value
+        self.parent = None
         self.children = []
         self.left = None
         self.right = None
@@ -77,8 +79,8 @@ class MyBinaryTreeNode(Node):
             self.right.print_in_order()
 
     # post order traversal --> L R S
-     def print_pre_order(self):
-            print self.value
+    def print_pre_order(self):
+        print self.value
 
         if self.left is not None:
             self.left.print_in_order()
@@ -95,6 +97,46 @@ class MyBinaryTreeNode(Node):
             self.right.print_in_order()
 
         print self.value
+
+    def is_root(self):
+        return self.parent is None
+
+    def is_right_child(self):
+        return self.value >= self.parent.value
+
+    def is_left_child(self):
+        if self.is_root(): return False
+        return self.value < self.parent.value
+
+    def rotate_left(self):
+        print "Starting Left Rotation"
+
+        # cannot perfrom rotate_left on root
+        if self.is_root():
+            return
+        # cannot perform rotate_left on left child
+        if self.is_left_child():
+            return
+
+        ogParent = self.parent
+        ogRight = self.left
+        pivot = self
+
+        ogParent.parent = pivot
+        pivot.left = ogParent
+
+        ogParent.right = ogLeft
+        ogLeft.parent = ogParent
+        return ogParent
+
+        # Step 0: Assign all the variables you'll be using
+        # Step 0.5: Check to see if you can actually do the rotation
+        #       a. Cannot rotate root.
+        #       b. Can only left rotate a right child
+        # Step 1: Pivot Becomes the ogParent's parent
+        # Step 2: THe ogLeftChild of the pivot becomes right child of ogParent
+        print "Done with Left rotation"
+        pass
 
 class BST:
     def __init__(self):
@@ -118,14 +160,45 @@ class BST:
     def height(self):
         if self.root is not None:
             return self.root.height()
+        return 0
 
+    def print_in_order(self):
+        if self.root is not None:
+            self.root.print_in_order()
+    
+    def print_level_order(self):
+        if self.root is None:
+            return
+
+        queue = deque()
+        queue.append(self.root)
+
+        while len(queue) != 0:
+            current_node = queue.popleft()
+            print current_node.value
+
+            if current_node.left is not None:
+                queue.append(current_node.left)
+
+            if current_node.right is not None:
+                queue.append(current_node.right)
+
+    def test_rotate():
+        if self.root is None or self.root.right is None:
+            return
+
+        ogParent = self.root.right.left_rotate()
+
+        if ogParent is self.root:
+            self.root = ogParent.parentq
 
 def random_numbers(total_numbers):
     return [int(1000 * nprnd.random()) for i in range(total_numbers)]
 
-
+node = MyBinaryTreeNode(1)
 tree = BST()
 for i in random_numbers(25):
     tree.add(i)
-show(tree.root)
-tree.print_level_order()
+# show(tree.root)
+# tree.print_level_order()
+node.rotate_left()
